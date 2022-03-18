eb007a7385c09318adb2532ca238593fd09eb296
Новокрещенова Алёна <91333734+Newisyou@users.noreply.github.com>
2022-03-18 23:12:59 +0500

    Add files via upload

---
+++ massive/ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector <double> mass1;
+    int I = 0;
+    double sum = 0;
+
+    ifstream file("in.txt");
+    if (!file)
+        cout << "problem" << endl;
+
+
+    while (!file.eof()) {
+        double a;
+        file >> a;
+        mass1.push_back(a);
+        I++;
+        sum += a;
+    }
+
+    cout << sum << endl;
+
+    sort(mass1.begin(), mass1.end());
+    for (int i = 0; i < mass1.size(); i++) {
+        cout << mass1[i] << "   ";
+    }
+    cout << endl;
+    cout << sum / mass1.size() << endl;
+};
---
+++ massive/ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿#include <ctime>
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    srand(time(0));
+    double sum = 0;
+    int mass[1000];
+    for (int i = 0; i < 1000; i++) {
+        int a = -100 + rand() % 201;
+        mass[i] = a;
+        sum += a;
+    }
+
+    for (int i = 0; i < 20; i++) {
+        cout << mass[i] << endl;
+    }
+
+    cout<<endl;
+    cout << sum << endl;
+    float result = sum / 1000;
+    cout <<result<<endl;
+}
+
